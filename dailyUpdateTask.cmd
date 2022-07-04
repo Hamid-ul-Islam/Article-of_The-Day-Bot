33dd241db8a99d3816aad67b74b4b6a2d54f188e
Hamid Chowdhury <hamidthedev@gmail.com>
2022-07-05 00:01:06 +0600

    Commited by Automated Bot

--- dailyUpdateTask.cmd
+++ dailyUpdateTask.cmd
@@ -1,43 +1,11 @@
 @ECHO OFF
 
-:: This CMD script provides you with your operating system, hardware and network information.
-
-TITLE My System Info
-
-ECHO Please wait... Gathering system information.
-
-ECHO =========================
-
-ECHO OPERATING SYSTEM
-
-systeminfo | findstr /c:"OS Name"
-
-systeminfo | findstr /c:"OS Version"
-
-ECHO =========================
-
-ECHO BIOS
-
-systeminfo | findstr /c:"System Type"
-
-ECHO =========================
-
-ECHO MEMORY
-
-systeminfo | findstr /c:"Total Physical Memory"
-
-ECHO =========================
-
-ECHO CPU
-
-wmic cpu get name
-
-ECHO =========================
-
-ECHO NETWORK ADDRESS
-
-ipconfig | findstr IPv4
-
-ipconfig | findstr IPv6
+:: This CMD script auto update data to github repository daily.
 
 python "D:\Development\Python\Wikipedia Article of the day\main.py"
+
+git add .
+
+git commit -m "Commited by Automated Bot"
+
+git push
