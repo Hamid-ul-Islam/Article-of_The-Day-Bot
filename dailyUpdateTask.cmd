749f99e7ea6f25c878279eed69c9a0a5a95182cb
Hamid Chowdhury <hamidthedev@gmail.com>
2022-08-23 20:11:36 +0600

    Commited by Automated Bot

--- dailyUpdateTask.cmd
+++ dailyUpdateTask.cmd
@@ -2,13 +2,12 @@
 
 :: This CMD script auto update and commit data to github repository daily.
 
-python "D:\Development\PYTHON\Final Projects\Wikipedia Article of the day\main.py"
+python "D:\Development\PYTHON\Final Projects\Article of the Day Wikipedia\Article-of_The-Day-Bot\main.py"
 
-cd "D:\Development\PYTHON\Final Projects\Wikipedia Article of the day\"
+cd "D:\Development\PYTHON\Final Projects\Article of the Day Wikipedia\Article-of_The-Day-Bot\"
 
 git add .
 
 git commit -m "Commited by Automated Bot"
 
 git push
-PAUSE
